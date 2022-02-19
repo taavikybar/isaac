d6dfffdf92023edcd66e8f670f90f9ce796975b6
Taavi Kybar <taavi.kubar@chordx.co>
2022-02-19 17:21:48 +0200

    read assets from db

--- helpers/collection.js
+++ helpers/collection.js
@@ -7,23 +7,15 @@ const db = require('./db')
 
 
 // private
-async function getCollection(colId) {
-  const data = await fs.readFileSync(`./assets/${colId}.json`, 'utf8')
-  return JSON.parse(data)
-}
+async function isAssetValid(a) {
+  const bids = await db.getBids(a.colId, a.id)
 
-async function getCheckedCollection(colId) {
-  const collection = await getCollection(colId)
-  return collection.filter(isAssetValid)
-}
-
-const isAssetValid = asset => {
-  if (asset.bids.length === 0) {
+  if (bids.length === 0) {
     return true
   }
 
   const MS_IN_H = 1000 * 60 * 60;
-  const lastBid = asset.bids[asset.bids.length - 1].date
+  const lastBid = bids[bids.length - 1].date
   const hoursSince = Math.floor((new Date() - new Date(lastBid)) / MS_IN_H)
 
   return hoursSince > c.bidDays * 24
@@ -34,25 +26,13 @@ const getUrl = (colId, assetId) => `${c.assetBaseUrl}/${getColById(colId).contra
 const getColById = id => c.collections.find(col => col.id === id)
 
 async function updateCollection(colId, assetId, bid) {
-  const collection = await getCollection(colId)
-  const index = collection.findIndex(a => a.id === assetId)
-
-  collection[index].bids.push({
-    date: new Date(),
-    bid,
-  })
-
-  const data = {
+  await db.addBid({
     date: new Date(),
     collectionId: a.colId,
     assetId: a.id,
     bid,
     worker: process.env.ID,
-  }
-  
-  db.addBid(data)
-
-  await fs.writeFileSync(`./assets/${colId}.json`, JSON.stringify(collection))
+  })
 }
 
 async function getAssets() {
@@ -60,8 +40,15 @@ async function getAssets() {
   const collections = c.collections.filter(c => c.worker === process.env.ID)
 
   for (col of collections) {
-    const checked = await getCheckedCollection(col.id)
-    checked.forEach(a => a.colId = col.id)
+    const allAssets = await db.getAssets(col.id)
+    const checked = []
+
+    for (a of allAssets) {
+      if (await isAssetValid(a)) {
+        checked.push(a)
+      }
+    }
+
     assets = [...assets, ...checked]
   }
 
--- helpers/db.js
+++ helpers/db.js
@@ -2,42 +2,77 @@ require('dotenv').config()
 let c = require('../constants')
 const log = require('./log');
 const DB_URL = `http://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}`
+const BIDS_TABLE = 'bids'
+const ASSETS_TABLE = 'assets'
 
 async function loadConfig() {
   try {
     const nano = require('nano')(DB_URL);
     const config = await nano.use('config')
     const global = await config.get('global')
-    const assets = await config.get('assets')
+    const collections = await config.get('collections')
 
     Object.keys(global).forEach(k => {
       c[k] = global[k]
     })
 
-    c.collections = assets.collections
+    c.collections = collections.collections
     log.info('Config loaded')
   } catch (e) {
-    log.info(`DB config load error: ${e}`)
+    log.info(`DB loadConfig error: ${e}`)
   }
 }
 
 async function addBid(data) {
   try {
     const nano = require('nano')(DB_URL);
-    const table = await nano.use('bids')
+    const table = await nano.use(BIDS_TABLE)
 
     await table.insert(data)
   } catch (e) {
-    log.info(`DB bid add error: ${e}`)
+    log.info(`DB addBid error: ${e}`)
   }
 }
 
-async function getAsset(colId, assetId) {
+async function getBids(colId, assetId) {
+  try {
+    const nano = require('nano')(DB_URL);
+    const table = await nano.use(BIDS_TABLE)
 
+    const docs = await table.find({
+      selector: {
+        collectionId: { $eq: colId },
+        assetId: { $eq: assetId },
+      }
+    })
+
+    return docs.docs
+  } catch (e) {
+    log.info(`DB getBids error: ${e}`)
+  }
 }
+
+async function getAssets(colId) {
+  try {
+    const nano = require('nano')(DB_URL);
+    const table = await nano.use(ASSETS_TABLE)
+    const ids = await table.get(colId)
+
+    return ids.ids.map(id => {
+      return {
+        id,
+        colId,
+      }
+    })
+  } catch (e) {
+    log.info(`DB getAsset error: ${e}`)
+  }
+}
+
 
 module.exports = {
   loadConfig,
   addBid,
-  getAsset,
+  getBids,
+  getAssets,
 }
